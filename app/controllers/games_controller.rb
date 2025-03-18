f4b640dc90b44e9da73e57105d77edb4c9da9d97
fieldstyler <tdfields93@gmail.com>
2025-03-17 21:07:29 -0700

    Add start action for games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,17 +6,39 @@ class GamesController < ApplicationController
     def show
         @game = Game.find(params[:id])
     end
-
+    
     def create
         @game = Game.create(games_params)
-        redirect_to single_game_blinds_path(@game.id)
+        redirect_to single_game_levels_path(@game.id)
     end
     
-    def blinds
+    def edit
         @game = Game.find(params[:id])
-        @levels = @game.level_count
+    end
+    
+    def update
+        @game = Game.find(params[:id])
+        levels = @game.level_count
+        length = @game.round_length
+        if levels == params["level_count"] && length == params["round_length"]
+            @game.update(games_params)
+            redirect_to games_show_path(@game.id)
+        else
+            @game.update(games_params)
+            redirect_to edit_single_game_levels_path(@game.id)
+        end
+    end
+    
+    def levels
+        @game = Game.find(params[:id])
+        @levels = @game.level_count.to
         @count = 1
         @length = @game.round_length
+    end
+    
+    def start
+        @game = Game.find(params[:id])
+        @count = 1
     end
     
     private
