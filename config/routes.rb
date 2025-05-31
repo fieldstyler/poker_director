e1014461ed26f31e14e421ebfc5bd96a1b0ae1db
fieldstyler <tdfields93@gmail.com>
2025-05-31 17:03:46 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,17 +14,85 @@ Rails.application.routes.draw do
 
   root "home#index"
 
-  get "/games/new" => "games#new", as: :new_single_game
-  get "/leagues/index" => "leagues#index"
-  get "/players/index" => "players#index"
-  post "/games/new/levels" => "games#create"
-  get "/games/new/levels/:id" => "games#levels", as: :single_game_levels
-  post "/levels/create" => "levels#create"
-  get "/games/show/:id" => "games#show", as: :games_show
-  get "games/edit/:id" => "games#edit", as: :edit_game
-  patch "games/update/:id" => "games#update", as: :update_game
-  get "/levels/edit/:id" => "levels#edit", as: :edit_single_game_levels
-  patch "games/levels/update/:id" => "levels#update", as: :levels_update
-  get "games/start/:id/level/:count" => "games#blinds", as: :game_blinds
-  get "games/start/:id/level/:count/break" => "games#break", as: :game_break
+    # == Players ==
+  get    "/players",          to: "players#index",   as: :players
+  get    "/players/new",      to: "players#new",     as: :new_player
+  post   "/players",          to: "players#create"
+  get    "/players/:id",      to: "players#show",    as: :player
+  get    "/players/:id/edit", to: "players#edit",    as: :edit_player
+  patch  "/players/:id",      to: "players#update"
+  delete "/players/:id",      to: "players#destroy"
+
+  # == Leagues ==
+  get    "/leagues",          to: "leagues#index",   as: :leagues
+  get    "/leagues/new",      to: "leagues#new",     as: :new_league
+  post   "/leagues",          to: "leagues#create"
+  get    "/leagues/:id",      to: "leagues#show",    as: :league
+  get    "/leagues/:id/edit", to: "leagues#edit",    as: :edit_league
+  patch  "/leagues/:id",      to: "leagues#update"
+  put    "/leagues/:id",      to: "leagues#update"
+  delete "/leagues/:id",      to: "leagues#destroy"
+  patch  "/leagues/:id/update_standings", to: "leagues#update_standings", as: :update_standings_league
+
+  # == Games ==
+  get    "/games/new",              to: "games#new",          as: :new_single_game
+  post   "/games/new/levels",      to: "games#create"
+  get    "/games/show/:id",        to: "games#show",         as: :games_show
+  get    "/games/edit/:id",        to: "games#edit",         as: :edit_game
+  patch  "/games/update/:id",      to: "games#update",       as: :update_game
+
+  # == LeagueGames ==
+  get  "/leagues/:league_id/games/new", to: "games#new_for_league", as: :new_league_game
+  post "/leagues/:league_id/games", to: "games#create_for_league", as: :league_games
+  get  "/leagues/:league_id/games/levels/:id", to: "games#levels_for_league", as: :league_games_levels
+  get "/leagues/:league_id/games/:id/edit", to: "games#edit_for_league", as: :edit_league_game
+  post "/leagues/:league_id/games/levels/create", to: "levels#create_for_league", as: :league_levels_create
+  patch "/leagues/:league_id/games/:id", to: "games#update_for_league", as: :update_league_game
+  get "/leagues/:league_id/games/:id/edit_levels", to: "levels#edit", as: :edit_league_game_levels
+  post "/leagues/:league_id/sessions/start", to: "games#start_session", as: :start_league_session
+
+  # == Levels (within Games) ==
+  get    "/games/new/levels/:id",     to: "games#levels",     as: :single_game_levels
+  get    "/levels/edit/:id",          to: "levels#edit",      as: :edit_single_game_levels
+  patch  "/games/levels/update/:id",  to: "levels#update",    as: :levels_update
+  post   "/levels/create",            to: "levels#create"
+
+  # == Game Flow ==
+  get    "/games/start/:id/level/:count",           to: "games#blinds", as: :game_blinds
+  get    "/games/start/:id/level/:count/break",     to: "games#break",  as: :game_break
+  get "/leagues/:league_id/games/:id/start/level/:count", to: "games#play_league_level", as: :league_game_blinds
+  get "/leagues/:league_id/games/:id/start/level/:count/break", to: "games#play_league_level_break", as: :league_game_break
+
+  post "/games/:id/knockout", to: "games#knockout", as: :knockout_game
+  post "/games/:id/complete", to: "games#complete_session", as: :complete_session_game
+
+  # get "/games/new" => "games#new", as: :new_single_game
+  # get "/leagues" => "leagues#index"
+  # get "/players/new" => "players#new", as: :new_player
+  # post "/players" => "players#create", as: :players
+  # get "/players/:id/edit" => "players#edit", as: :edit_player
+  # get "/players/:id" => "players#show", as: :player
+  # patch "/players/:id" => "players#update"
+  # get "/players" => "players#index"
+  # delete "/players/:id" => "players#destroy"
+  # post "/games/new/levels" => "games#create"
+  # get "/games/new/levels/:id" => "games#levels", as: :single_game_levels
+  # post "/levels/create" => "levels#create"
+  # get "/games/show/:id" => "games#show", as: :games_show
+  # get "games/edit/:id" => "games#edit", as: :edit_game
+  # patch "games/update/:id" => "games#update", as: :update_game
+  # get "/levels/edit/:id" => "levels#edit", as: :edit_single_game_levels
+  # patch "games/levels/update/:id" => "levels#update", as: :levels_update
+  # get "games/start/:id/level/:count" => "games#blinds", as: :game_blinds
+  # get "games/start/:id/level/:count/break" => "games#break", as: :game_break
+
+  # get     "/leagues",          to: "leagues#index",   as: :leagues
+  # get     "/leagues/new",      to: "leagues#new",     as: :new_league
+  # post    "/leagues",          to: "leagues#create"
+  # get     "/leagues/:id",      to: "leagues#show",    as: :league
+  # get     "/leagues/:id/edit", to: "leagues#edit",    as: :edit_league
+  # patch   "/leagues/:id",      to: "leagues#update"
+  # put     "/leagues/:id",      to: "leagues#update"
+  # delete  "/leagues/:id",      to: "leagues#destroy"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_28_043812) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_31_160119) do
+  create_table "game_players", force: :cascade do |t|
+    t.integer "game_id", null: false
+    t.integer "player_id", null: false
+    t.integer "points", default: 0
+    t.integer "knockouts", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "eliminated", default: false
+    t.integer "session_points", default: 0
+    t.integer "session_knockouts", default: 0
+    t.boolean "bounty", default: false
+    t.integer "eliminated_by_id"
+    t.index ["game_id"], name: "index_game_players_on_game_id"
+    t.index ["player_id"], name: "index_game_players_on_player_id"
+  end
+
   create_table "games", force: :cascade do |t|
     t.string "buy_in"
     t.string "starting_stack"
@@ -21,6 +37,28 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_28_043812) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "rebuys"
+    t.integer "league_id"
+    t.boolean "completed", default: false
+    t.integer "player_count"
+    t.index ["league_id"], name: "index_games_on_league_id"
+  end
+
+  create_table "league_players", force: :cascade do |t|
+    t.integer "player_id", null: false
+    t.integer "league_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "season_points", default: 0
+    t.integer "season_knockouts", default: 0
+    t.index ["league_id"], name: "index_league_players_on_league_id"
+    t.index ["player_id"], name: "index_league_players_on_player_id"
+  end
+
+  create_table "leagues", force: :cascade do |t|
+    t.string "name"
+    t.integer "number_of_sessions"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "levels", force: :cascade do |t|
@@ -36,5 +74,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_28_043812) do
     t.index ["game_id"], name: "index_levels_on_game_id"
   end
 
+  create_table "players", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "game_players", "games"
+  add_foreign_key "game_players", "players"
+  add_foreign_key "games", "leagues"
+  add_foreign_key "league_players", "leagues"
+  add_foreign_key "league_players", "players"
   add_foreign_key "levels", "games"
 end
